dcd2095f42840c5880192b613376f06b7b6f1ed5
Sagnik Datta <sagnikdatta@Sagniks-MacBook-Air.local>
2024-08-01 18:42:23 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,48 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import { useState } from "react";
 export default function Home() {
+  const [rows, setRows] = useState<number>(4);
+  const [columns, setColumns] = useState<number>(4);
+  const [tables, setTables] = useState([]);
+  const [tables1, setTable1] = useState([]);
+  const generateTable = () => {
+    const newtable = [];
+
+    const table = (
+      <table className="table">
+        <tbody>
+          {Array.from({ length: rows }).map((_, rowIndex) => (
+            <tr>
+              {Array.from({ length: columns }).map((_, colIndex) => (
+                <td>{rowIndex + colIndex}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+
+    newtable.push(table);
+
+    const table1 = (
+      <table className="table">
+        <tbody>
+          {Array.from({ length: rows }).map((_, rowIndex) => (
+            <tr>
+              {Array.from({ length: columns }).map((_, colIndex) => (
+                <td>{rowIndex * colIndex}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+    newtable.push(table1);
+
+    setTables(newtable);
+  };
+
   return (
     <>
       <Head>
@@ -14,99 +51,32 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className={`${styles.main}`}>
+        <div className="container">
+          <div className="inputs">
+            <label htmlFor="rows">Rows</label>
+            <input
+              type="number"
+              id="rows"
+              name="rows"
+              min="1"
+              value={rows}
+              onChange={(e) => setRows(parseInt(e.target.value))}
+            />
+            <label htmlFor="columns">Columns</label>
+            <input
+              type="number"
+              id="columns"
+              name="columns"
+              min="1"
+              value={columns}
+              onChange={(e) => setColumns(parseInt(e.target.value))}
+            />
+            <button onClick={generateTable} className="generate-btn">
+              Generate Tables
+            </button>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          <div className="table-container">{tables}</div>
         </div>
       </main>
     </>
